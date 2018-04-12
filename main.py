942628e516073c2656032e8e4c12a7d6425723fb
zengruijin <zengruijin@cmhi.chinamobile.com>
2018-04-12 13:38:04 +0800

    Update: 1 增加iOS图标； 2 增加linux服务器登录 & 传输结果图片；

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from ftplib import FTP
 import pandas as pd
+import paramiko
+
+#"%matplotlib inline"
 import matplotlib.pyplot as plt
+import os
 
 # Richard Zeng Created this file on 2018-04-08.
 # For CMCC APK Size dynamic checking and visulization.
@@ -8,8 +12,28 @@ import matplotlib.pyplot as plt
 #FTP host address, no port
 FTP_HOST = '172.23.12.64'
 #Root file path we need
-ROOT_FILE_PATH = '/zdyy/UniApp/dev'
+ROOT_ANDROID_FILE_PATH = '/zdyy/UniApp/dev'
+ROOT_IOS_FILE_PATH = '/zdyy/UniversalApp/git-develop'
 FILD_DIV_SIMBOL = '/'
+
+APK_SIZE_RESULT_PNG = 'apksize.png'
+IPA_SIZE_RESULT_PNG = 'ipasize.png'
+
+#Linux 服务器信息
+#服务器ip
+Host='172.23.25.205'
+Port=22
+#登录用户名
+Username='root'
+#登录密码
+Password='tvgATWXj'
+#登录服务器后执行的命令
+Command = ['cd /usr/local/cmcc/apache-tomcat-7.0.75/webapps/appsize']
+#本地PC路径
+# CUR_PATH = os.path.dirname(__file__)
+WinPath = r'./apksize.png'
+#服务器上的路径
+LinuxPath = r'/usr/local/cmcc/apache-tomcat-7.0.75/webapps/apksize/%s'
 
 #init FTP client & goto the daily folder
 def init_ftp():
@@ -18,7 +42,6 @@ def init_ftp():
         ftp = FTP()
         ftp.connect(FTP_HOST)
         ftp.login('zdyy', 'zdFTP@11')
-        ftp.cwd(ROOT_FILE_PATH)
         print(ftp.getwelcome())
     except e:
         print('Failed to connect with FTP "%s"' % FTP_HOST)
@@ -27,7 +50,11 @@ def init_ftp():
     return ftp
 
 #Get dev folder, we just analyze the everyday's first APK package.
-def get_daily_build_infor(ftp):
+def get_daily_build_infor(ftp, rootPath):
+
+    #Goto root file path
+    ftp.cwd(rootPath)
+
     folderResult = []
     apkSize = []
     filelist = []
@@ -35,7 +62,7 @@ def get_daily_build_infor(ftp):
     for f in filelist:
         #Check dir or file
         if f[0] == 'd':
-            size =  get_daily_build_apk_size(ftp, ROOT_FILE_PATH + FILD_DIV_SIMBOL + f.split()[-1])
+            size =  get_daily_build_apk_size(ftp, rootPath + FILD_DIV_SIMBOL + f.split()[-1])
             if size > 0:
                 folderResult.append(f.split()[-1])
                 apkSize.append(size)
@@ -53,7 +80,9 @@ def get_daily_build_apk_size(ftp, folderPath):
     for f in files:
         # print(f)
         #Check dir or file
-        if f[0] != 'd' and f.find('dev') >= 0:
+        if f[0] != 'd' \
+           and f.find('dev') >= 0 \
+           and (f.find('.apk') >= 0 or f.find('.ipa') >= 0):
             #Convert 2 Kb from bytes.
             # print(f.split()[4])
             return int((int(f.split()[4]) / 1024))
@@ -69,28 +98,78 @@ def create_apk_size_df(row):
     print(df)
     return df
 
-def show_apk_size_bar(data):
+def show_apk_size_bar(data, fileName):
+    #plt.figure(figsize=(30, 30))
+
     data.plot(kind='bar', stacked=True, alpha=0.7)
-    plt.xlabel('Date')
-    plt.ylabel('APK Size (Kb)')
-    plt.title('APK Size Dynamic Chart')
-    plt.show()
+    # plt.xticks(rotation=45)
+    # plt.xlabel('Date')
+    plt.ylabel('APP Size (Kb)')
+    plt.title('APP Size Dynamic Chart')
+
+    plt.subplots_adjust(left=0.15, wspace=0.25, hspace=0.25,
+                        bottom=0.18, top=0.91)
+
+    # plt.show()
+    if(os.path.exists(fileName)):
+        os.remove(fileName)
+
+    plt.tight_layout()
+    plt.savefig(fileName)
+
+    if (os.path.exists(fileName)):
+        conn_linux_and_trans_png(fileName)
+        print("Target app size png is transferred.")
+    else:
+        print('Err: Target app size png is incorrect!')
 
 def show_apk_size_diff(data):
     plt.plot(data.index, data.diff(), c='red')
     # data.diff(axis=0).dropna().plot(stacked=True, alpha=0.7)
-    plt.xticks(rotation=90)
+
+    plt.xticks(rotation=45)
     plt.xlabel('Date')
     plt.ylabel('APK Diff Size (Kb)')
     plt.title('APK Size Diff Dynamic Chart')
-    plt.show()
+    plt.savefig('apksize_diff.png')
 
-ftp = init_ftp()
-row_df = get_daily_build_infor(ftp)
-df = create_apk_size_df(row_df)
+# def copy_file():
+#     targetPath = '/usr/local/cmcc/apache-tomcat-7.0.75/webapps/apksize'
+#     if(os.path.exists(targetPath) and os.path.exists(APK_SIZE_RESULT_PNG)):
+#         val = os.system("cp ./%s %s" % (APK_SIZE_RESULT_PNG, targetPath))
+#         print(val)
+#     else:
+#         print('Target apk size png or target copy path is incorrect.')
 
-#Apk size bar chart
-show_apk_size_bar(df)
+def main():
+    ftp = init_ftp()
 
-#We dont' need diff chart now.
-# show_apk_size_diff(df)
+    #Android information.
+    anroid_row_df = get_daily_build_infor(ftp, ROOT_ANDROID_FILE_PATH)
+    df = create_apk_size_df(anroid_row_df)
+    # Apk size bar chart
+    show_apk_size_bar(df, APK_SIZE_RESULT_PNG)
+
+    #iOS information
+    ios_row_df = get_daily_build_infor(ftp, ROOT_IOS_FILE_PATH)
+    df = create_apk_size_df(ios_row_df)
+    # Apk size bar chart
+    show_apk_size_bar(df, IPA_SIZE_RESULT_PNG)
+
+    # We dont' need diff chart now.
+    # show_apk_size_diff(df)
+
+def conn_linux_and_trans_png(fileName):
+    '''SSHA远程登录：Windows客户端连接Linux服务器，并输入指令'''
+
+    client = paramiko.Transport((Host, Port))
+    client.connect(username=Username, password=Password)
+    sftp = paramiko.SFTPClient.from_transport(client)
+
+    sftp = paramiko.SFTPClient.from_transport(client)
+    sftp.put(r'./%s' % (fileName), r'/usr/local/cmcc/apache-tomcat-7.0.75/webapps/apksize/%s' % (fileName))
+    #执行完毕，终止连接
+    client.close()
+
+if __name__ == '__main__':
+    main()
